7d2bd78bc90445d8395b238629473199ba364fcc
Valdis <valdisxp1@gmail.com>
2014-07-25 09:01:49 +0300

    properties macro

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -1,15 +1,33 @@
 import scala.reflect.macros.Context
 import scala.language.experimental.macros
+import java.util.Properties
+import java.io.FileInputStream
 
 object Macro{
 def loadFile(file:String):String = macro loadFileImpl
 def loadFileImpl(c:Context)(file:c.Expr[String])={
   import c.universe._
-  val Literal(Constant(file_name: String)) = file.tree
-  val source = scala.io.Source.fromFile(file_name)
+  val Literal(Constant(fileName: String)) = file.tree
+  val source = scala.io.Source.fromFile(fileName)
   val contents = source.mkString
   source.close()
 
   Literal(Constant(contents))
+}
+
+def loadProperties(file:String):Map[String,String] = macro loadPropertiesImpl
+def loadPropertiesImpl(c:Context)(file:c.Expr[String])={
+  import c.universe._
+  import scala.collection.JavaConverters._
+  val Literal(Constant(fileName: String)) = file.tree
+  val fis = new FileInputStream(fileName)
+  val properties = new Properties
+  properties.load(fis)
+  fis.close
+  
+  val map = properties.asScala
+  
+  //c.Expr[Map[String,String]]
+  reify(Map.empty[String,String])
 }
 }
