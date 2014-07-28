9818dd52db4e7e8c2bbb7a2aecef4b854ac32021
Valdis <valdisxp1@gmail.com>
2014-07-28 11:50:40 +0300

    q quotes FTW

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -26,24 +26,14 @@ def loadPropertiesImpl(c: Context)(file: c.Expr[String])={
   properties.load(fis)
   fis.close
   
-  val map = properties.asScala
+  val propertiesMap = properties.asScala
   
-  def constant(s:String)=c.Expr[String](Literal(Constant(s)))
-  val pairs = map.map{case(k,v)=>reify((constant(k).splice,constant(v).splice)).tree}
-  
-  val mapVariable= newTermName("map")
-  
-  val typeTag = typeOf[mutable.Builder[(String,String),Map[String,String]]]
-  val define = ValDef(Modifiers(), mapVariable,TypeTree(typeTag) , reify(Map.newBuilder[String,String]).tree)
-  
-  val result = Apply(Select(Ident(mapVariable), newTermName("result")), List())
-  
-  def add(arg: Tree): Apply = Apply(Select(Ident(mapVariable), newTermName("$plus$eq")), List(arg))
-  
-  val adds = pairs.map(t=>add(t)).toList
+  val defineBuilder = q"val map=Map.newBuilder[String,String]"
+  val adds = propertiesMap.map{case(k,v)=>q"map += (($k,$v))"}.toList
+  val result = q"map.result"
   
   c.Expr[Map[String,String]](Block(
-  define :: adds, result
+  defineBuilder :: adds, result
   ))
 }
 }
