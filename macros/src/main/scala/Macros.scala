2a2770782264fc7697736c56603c98c471cb4819
Valdis <valdisxp1@gmail.com>
2014-07-28 15:08:32 +0300

    meh

--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -19,7 +19,6 @@ def loadProperties(file: String): Map[String,String] = macro loadPropertiesImpl
 def loadPropertiesImpl(c: Context)(file: c.Expr[String])={
   import c.universe._
   import scala.collection.JavaConverters._
-  import scala.collection.mutable
   val Literal(Constant(fileName: String)) = file.tree
   val fis = new FileInputStream(fileName)
   val properties = new Properties
@@ -31,9 +30,34 @@ def loadPropertiesImpl(c: Context)(file: c.Expr[String])={
   val adds = propertiesMap.map{case(k,v)=>q"map += (($k,$v))"}.toList
 
   q"""
-	val map=Map.newBuilder[String,String]
+	val map = Map.newBuilder[String,String]
 	$adds
 	map.result
   """
+}
+
+def divC(a:Int,d:Int):Int= macro divByConstImpl
+def divByConstImpl(c: Context)(a: c.Expr[Int],d:c.Expr[Int])={
+	import c.universe._
+	val Literal(Constant(dVal: Int)) = d.tree
+	if(dVal<=0){
+		c.abort(c.enclosingPosition, "Only positive values supported")
+	}
+	
+	val allPowersOf2=Stream.iterate(1)(_<<1).takeWhile(_<=dVal)
+	val powersOf2 =allPowersOf2.filter((x:Int)=> (x & dVal) != 0).map(_-1).toList
+
+	val result= powersOf2.map{
+		case 0 => q"$a"
+		case x => q"$a >> $x"
+	}.reduceLeft((left,right)=>q"$left+$right")
+	
+	println(result)
+	
+	result
+	/*q"""
+	$a/$dVal
+	"""
+	*/
 }
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,4 +2,5 @@ object Main extends App{
 val file = Macro loadFile "src\\main\\scala\\Main.Scala"
 val properties = Macro loadProperties "some.properties"
 println(properties)
+println("20/3="+ Macro.divC(20,3))
 }
