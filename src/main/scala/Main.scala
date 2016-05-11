096a4d63dad6669caae34fa29f114cb41c8e424b
Valdis Adamsons <valdisxp1@gmail.com>
2016-05-11 13:05:42 +0300

    fix case sensetive path

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
-object Main extends App{
-val file = Macro loadFile "src\\main\\scala\\Main.Scala"
-val properties = Macro loadProperties "some.properties"
-println(properties)
-println("20/3="+ Macro.divC(20,3))
+object Main extends App {
+  val file = Macro loadFile "src/main/scala/Main.scala"
+  val properties = Macro loadProperties "some.properties"
+  println(properties)
+  println("20/3=" + Macro.divC(20, 3))
 }
