1944e8cd08e740e344518e576b4cb25cc8956030
Valdis Adamsons <valdisxp1@gmail.com>
2016-05-11 13:18:48 +0300

    some comments

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,14 @@
 object Main extends App {
+  // use a macro to copy contents of a text file as a constant in the code
   val file = Macro loadFile "src/main/scala/Main.scala"
+  println(s"Source for this file is stored in the compiled code: \n$file\n---")
+
+  // load properties into a map constant
   val properties = LoadProperties loadProperties "some.properties"
   println(properties)
-  println("20/3=" + Macro.divC(20, 3))
+
+  // divide by a constant by only using bitshifts and adds
+  println(s"20/3=${Macro.divC(20, 3)}")
+  val x = 20
+  println(s"20/3=${Macro.divC(x, 3)}")
 }
