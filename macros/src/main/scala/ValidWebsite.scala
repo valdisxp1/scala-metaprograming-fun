9b676df2de0c2133279b83b7bcff8d4135afcf45
Valdis <valdisxp1@gmail.com>
2016-05-13 16:21:07 +0300

    starting the string interpolation macro

--- macros/src/main/scala/ValidWebsite.scala
+++ macros/src/main/scala/ValidWebsite.scala
@@ -23,7 +23,34 @@ object ValidWebsite {
     q"new java.net.URL($urlValue)"
   }
 
-  /*implicit class Website(stringContext: StringContext){
-    def website() = macro validWebsiteImpl
-  }*/
+  implicit class Website(stringContext: StringContext){
+    @inline def website(args: Any*): URL = macro validWebsiteImpl2
+  }
+
+  def validWebsiteImpl2(c: Context)(args: c.Expr[Any]*) = {
+    import c.universe._
+    val urlValue = c.prefix.tree match {
+      case Apply(_, List(Apply(_, partTrees))) =>
+        val parts = partTrees map { case q"${const: String}" => const }
+        parts match {
+          case List(url) if args.isEmpty => url
+          case _ => c.abort(c.enclosingPosition,"Only a single string value is supported")
+        }
+      case _ =>  c.abort(c.enclosingPosition, "invalid")
+    }
+
+    try {
+      val parsed = new URL(urlValue)
+      if (parsed.getProtocol != "http" && parsed.getProtocol != "https") {
+        c.abort(c.enclosingPosition, s"$urlValue isn't a website! It must be http(s)!")
+      } else {
+        c.info(c.enclosingPosition, s"$urlValue is a nice website", force = false)
+      }
+    } catch {
+      case t: Throwable =>
+        c.abort(c.enclosingPosition, s"Exception during parsing $t")
+    }
+    q"new java.net.URL($urlValue)"
+  }
+
 }
